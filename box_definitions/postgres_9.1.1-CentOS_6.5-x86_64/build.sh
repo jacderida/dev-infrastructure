24472d2df53db5ff0e0795b8c38d3ee1969c6ef7
Chris O'Neil <chris.oneil@gmail.com>
2014-08-16 23:47:07 +0100

    Introduce dev_infra prefix

--- box_definitions/postgres_9.1.1-CentOS_6.5-x86_64/build.sh
+++ box_definitions/postgres_9.1.1-CentOS_6.5-x86_64/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+prefix="dev_infra-"
 image_name=$(basename `pwd`)
+full_image_name=$prefix$image_name
 function get_self_owned_aws_images()
 {
     aws ec2 describe-images --owners self --output json
@@ -8,15 +10,15 @@ function get_self_owned_aws_images()
 
 function get_image_id_for_name()
 {
-    jq --raw-output --arg image_name $image_name '.Images | map(select(.Name == $image_name)) | .[]["ImageId"]'
+    jq --raw-output --arg image_name $full_image_name '.Images | map(select(.Name == $image_name)) | .[]["ImageId"]'
 }
 
 function remove_existing_image()
 {
     image_id=$(get_self_owned_aws_images | get_image_id_for_name)
-    if [[ ! -z "$image_id" ]]; then
-        echo "Removing existing $image_name AMI"
-        aws ec2 deregister-image --image-id $image_id
+    if [[ ! -z "$full_image_name" ]]; then
+        echo "Removing existing $full_image_name AMI"
+        aws ec2 deregister-image --image-id $full_image_name
     fi
 }
 
