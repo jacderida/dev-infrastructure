03b14f6d6a87462c422f85402dbe105dbafc7ff6
Chris O'Neil <chris.oneil@gmail.com>
2014-08-21 22:31:57 +0100

    Parameterize ec2_user class with authorized key
    This isn't an ideal solution, but it allows the reuse of the user with
    different keys. Perhaps I should be looking at using Amazon Linux base
    AMIs, which would do away with the manual key management pain.

--- modules/role/manifests/database_server.pp
+++ modules/role/manifests/database_server.pp
@@ -1,4 +1,6 @@
 class role::database_server inherits role {
-  include user::ec2_user
+  class { 'user::ec2_user':
+    authorized_key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCiiQ1rHZZNQu11o6IejtxZi8s1Snke9oWeOmF+phkmTUjv2VSQljK+2MEH+iv/Du5VFXsBStgOSlQ1PdcbnlwIObDZIstAtrpBnDjh/BT8/0DTSB//6wZgkLuxCGluK2BBLZxDe3f39WK3A9Tt2Dh/zl/VG0JETRmZLQ46OxIqNlSlP8Ow+Zn/RGEfoqfSEy5YnKb309uY8BZ21rRfMG32Z1oCPqEc7wzkaWoGdi9mq98hNB2197TqV5OJLt+BiIeTrtFhpdZMCpFZy+0iWFuTog738rrC/dqDeIToTj6YomIWl44i8VRjGLszDb8rbJGkb3di8Zj93j9T8CIzjq65'
+  }
   include profile::database
 }
