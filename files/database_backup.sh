a46fdc98e63c5f2f3eb7a99f993804be5fd2b1ce
Chris O'Neil <chris.oneil@gmail.com>
2014-08-27 21:24:45 +0100

    Add cron job to backup database

--- files/database_backup.sh
+++ files/database_backup.sh
@@ -3,4 +3,4 @@
 backups_path="/srv/backups"
 pg_dump -U postgres jira > "$backups_path/jira_$(date +%d%m%y%k%M%S)"
 backup_file=$(basename `ls $backups_path -t | head -n 1`)
-aws s3 cp $backups_path/$backup_file s3://jacderida-postgres-backups/$backup_file
+aws s3 cp $backups_path/$backup_file s3://jacderida-jira-backups/$backup_file
--- modules/profile/manifests/database.pp
+++ modules/profile/manifests/database.pp
@@ -5,6 +5,17 @@ class profile::database {
   $jira_dbuser = hiera('profile::database::jira_dbuser')
   $jira_dbuser_password = hiera('profile::database::jira_dbuser_password')
   $jira_db_encoding = hiera('profile::database::jira_db_encoding')
+  $cron_command = hiera('profile::database::cron_command')
+  $cron_minute = hiera('profile::database::cron_minute')
+  $cron_hour = hiera('profile::database::cron_hour')
+  $cron_date = hiera('profile::database::cron_date')
+  $cron_month = hiera('profile::database::cron_month')
+  $cron_weekday = hiera('profile::database::cron_weekday')
+  $cron_user = hiera('profile::database::cron_user')
+  $cron_backup_path = hiera('profile::database::cron_backup_path')
+  $cron_path_owner = hiera('profile::database::cron_path_owner')
+  $cron_path_group = hiera('profile::database::cron_path_group')
+  $cron_path_mode = hiera('profile::database::cron_path_mode')
 
   firewall { "120 allow access on ${port}":
     port   => $port,
@@ -45,4 +56,26 @@ class profile::database {
     encoding => $jira_db_encoding
   } ->
   anchor { 'postgres::end': }
+
+  file { $cron_backup_path:
+    ensure => 'directory',
+    owner  =>  $cron_path_owner,
+    group  =>  $cron_path_group,
+    mode   =>  $cron_path_mode,
+  } ->
+
+  file { "${cron_backup_path}/postgres_backup.sh":
+    ensure => present,
+    source => 'puppet:///files/postgres_backup.sh'
+  } ->
+
+  cron::job { 'postgres_backup':
+    minute  => $cron_minute,
+    hour    => $cron_hour,
+    date    => $cron_date,
+    month   => $cron_month,
+    weekday => $cron_weekday,
+    user    => $cron_user,
+    command => $cron_command
+  }
 }
