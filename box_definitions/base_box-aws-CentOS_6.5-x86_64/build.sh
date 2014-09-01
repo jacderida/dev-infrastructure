950501701859427f829b93364e788f103866c33d
Chris O'Neil <chris.oneil@gmail.com>
2014-09-02 00:59:26 +0100

    Pass the image name as a parameter

--- box_definitions/base_box-aws-CentOS_6.5-x86_64/build.sh
+++ box_definitions/base_box-aws-CentOS_6.5-x86_64/build.sh
@@ -24,7 +24,7 @@ function remove_existing_image()
     image_id=$(get_self_owned_aws_images | get_image_id_for_name)
     if [[ ! -z "$image_id" ]]; then
         echo "Removing existing $full_image_name AMI"
-        aws ec2 deregister-image --image-id $full_image_name
+        aws ec2 deregister-image --image-id $image_id
     fi
 }
 
@@ -43,4 +43,4 @@ function create_security_group()
 
 remove_existing_image
 create_security_group
-packer build template.json
+packer build -var 'image_name='"$full_image_name"'' template.json
