216a5b588e71d658aeb92cb110f0812fd95cdd41
Chris O'Neil <chris.oneil@gmail.com>
2014-09-13 22:28:51 +0100

    Add backup cron job for jenkins

--- modules/profile/manifests/jenkins_master.pp
+++ modules/profile/manifests/jenkins_master.pp
@@ -4,6 +4,18 @@ class profile::jenkins_master {
   $port = hiera('profile::jenkins_master::port')
   $plugins = hiera('profile::jenkins_master::plugins')
 
+  $cron_command = hiera('profile::jenkins_master::cron_command')
+  $cron_minute = hiera('profile::jenkins_master::cron_minute')
+  $cron_hour = hiera('profile::jenkins_master::cron_hour')
+  $cron_date = hiera('profile::jenkins_master::cron_date')
+  $cron_month = hiera('profile::jenkins_master::cron_month')
+  $cron_weekday = hiera('profile::jenkins_master::cron_weekday')
+  $cron_user = hiera('profile::jenkins_master::cron_user')
+  $cron_backup_path = hiera('profile::jenkins_master::cron_backup_path')
+  $cron_path_owner = hiera('profile::jenkins_master::cron_path_owner')
+  $cron_path_group = hiera('profile::jenkins_master::cron_path_group')
+  $cron_path_mode = hiera('profile::jenkins_master::cron_path_mode')
+
   firewall { "120 allow http and https access on ${port}":
     port   => $port,
     proto  => tcp,
@@ -17,6 +29,28 @@ class profile::jenkins_master {
     install_java => $install_java,
     plugin_hash  => $plugins,
     config_hash  => { 'JENKINS_PORT' => { 'value' => $port } }
+  } ->
+
+  file { $cron_backup_path:
+    ensure => 'directory',
+    owner  =>  $cron_path_owner,
+    group  =>  $cron_path_group,
+    mode   =>  $cron_path_mode,
+  } ->
+
+  file { "${cron_backup_path}/jenkins_backup.sh":
+    ensure => present,
+    content => template('profile/jenkins_backup.sh.erb')
+  } ->
+
+  cron::job { 'jira_backup':
+    minute  => $cron_minute,
+    hour    => $cron_hour,
+    date    => $cron_date,
+    month   => $cron_month,
+    weekday => $cron_weekday,
+    user    => $cron_user,
+    command => $cron_command
   } ->
 
   # This is a temporary measure to allow the userContent directory time to be created.
