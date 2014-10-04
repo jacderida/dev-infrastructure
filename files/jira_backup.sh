9c85d25b0239736dadef53420f1b517e847d5d36
Chris O'Neil <chris.oneil@gmail.com>
2014-10-05 00:36:49 +0100

    More comprehensive backup for JIRA

--- files/jira_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-backups_path="/srv/backups"
-backup_file="jira_home_$(date +%d%m%y%k%M%S).tar"
-tar -cvf $backup_file /home/jira
-mv $backup_file $backups_path
-aws s3 cp $backups_path/$backup_file s3://jacderida-jira-backups/$backup_file
